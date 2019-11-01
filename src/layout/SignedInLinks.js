79cc2c8edf2c06442be24896911edea411f98404
Dillon Coffman <tug40394@temple.edu>
2019-11-01 15:58:08 -0400

    Set up Navbar, SignedInLinks, and SignedOutLinks, adjusted MaterializeCSS CDN

---
+++ src/layout/SignedInLinks.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const SignedInLinks = () => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/">New Review</NavLink>
+      </li>
+      <li>
+        <NavLink to="/">Log Out</NavLink>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating light-blue lighten-4">
+          DC
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default SignedInLinks;
