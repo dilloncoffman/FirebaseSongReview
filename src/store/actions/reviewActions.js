8237c5fe3acc76bf029f122ddc3b533c46e3e720
Dillon Coffman <tug40394@temple.edu>
2019-11-01 18:00:03 -0400

    Enhanced store by connecting redux-firestore and react-redux-firebase using fbConfig

--- src/store/actions/reviewActions.js
+++ src/store/actions/reviewActions.js
@@ -1,5 +1,5 @@
 export const createSongReview = review => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to db
     dispatch({ type: "CREATE_SONG_REVIEW", review: review });
   }; // thunk allows action to return function
