382e126cfbd1ebcd648b057009db16fb6cbe0a9a
Dillon Coffman <tug40394@temple.edu>
2019-11-01 16:32:09 -0400

    Created SignIn, SignUp components, added basic state with handler functions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import Home from "./home/Home";
 import SongReviewDetails from "./song-reviews/SongReviewDetails";
+import SignIn from "./auth/SignIn";
+import SignUp from "./auth/SignUp";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/song-review/:id" component={SongReviewDetails} />
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/sign-up" component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
