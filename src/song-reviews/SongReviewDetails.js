9c3ff6ee9854a845045243675ee98fb13e6e6cc1
Dillon Coffman <tug40394@temple.edu>
2019-11-02 09:17:35 -0400

    Connected Firestore data to SongReviewDetails

--- src/song-reviews/SongReviewDetails.js
+++ src/song-reviews/SongReviewDetails.js
@@ -1,26 +1,44 @@
 import React from "react";
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
 
 const SongReviewDetails = props => {
-  const id = props.match.params.id;
-  return (
-    <div className="container section song-review-details">
-      <div className="card z-depth-0">
-        <div className="card-contet">
-          <span className="card-title">Song Review - {id}</span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
-            quasi et voluptatibus, ut rem quam autem expedita officiis fuga qui
-            eos voluptates labore soluta voluptatum iure culpa, commodi
-            recusandae laborum.
-          </p>
-          <div className="card-action grey lighten-4 grey-text">
-            <div>Reviewed by Dillon Coffman</div>
-            <div>Nov. 1st, 2019</div>
+  const { review } = props;
+  if (review) {
+    return (
+      <div className="container section song-review-details">
+        <div className="card z-depth-0">
+          <div className="card-contet">
+            <span className="card-title">{review.songName}</span>
+            <p>{review.reviewContent}</p>
+            <div className="card-action grey lighten-4 grey-text">
+              <div>Reviewed by {review.authorUser}</div>
+              <div>Nov. 1st, 2019</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container center">
+        <h3 className="white-text">Loading review...</h3>
+      </div>
+    );
+  }
 };
 
-export default SongReviewDetails;
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  const reviews = state.firestore.data.reviews;
+  const review = reviews ? reviews[id] : null;
+  return {
+    review: review
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "reviews" }])
+)(SongReviewDetails);
--- src/song-reviews/SongReviewList.js
+++ src/song-reviews/SongReviewList.js
@@ -1,12 +1,17 @@
 import React from "react";
 import SongReviewExcerpt from "./SongReviewExcerpt";
+import { Link } from "react-router-dom";
 
 const SongReviewList = ({ reviews }) => {
   return (
     <div className="song-review-list section">
       {reviews &&
         reviews.map(review => {
-          return <SongReviewExcerpt review={review} key={review.id} />;
+          return (
+            <Link key={review.id} to={"/song-review/" + review.id}>
+              <SongReviewExcerpt review={review} key={review.id} />;
+            </Link>
+          );
         })}
     </div>
   );
