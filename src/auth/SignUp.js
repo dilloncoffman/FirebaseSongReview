c0ae65457b82ccb9882793169b091e31b8004abd
Dillon Coffman <tug40394@temple.edu>
2019-11-02 16:37:25 -0400

    Added file uploader on sign up that accepts a user's file and uploads it to the appropriate bucket under directory profilePhotos, but have not figured out how to use this within state

--- src/auth/SignUp.js
+++ src/auth/SignUp.js
@@ -2,12 +2,18 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { signUp } from "../store/actions/authActions";
+import firebase from "firebase";
+import FileUploader from "react-firebase-file-uploader";
 
 class SignUp extends Component {
   state = {
     username: "",
     email: "",
-    password: ""
+    password: "",
+    avatar: "",
+    isUploading: false,
+    progress: 0,
+    avatarURL: ""
   };
 
   handleChange = e => {
@@ -21,15 +27,42 @@ class SignUp extends Component {
     this.props.signUp(this.state);
   };
 
+  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
+  handleProgress = progress => this.setState({ progress });
+  handleUploadError = error => {
+    this.setState({ isUploading: false });
+    console.error(error);
+  };
+  handleUploadSuccess = filename => {
+    this.setState({ avatar: filename, progress: 100, isUploading: false });
+    firebase
+      .storage()
+      .ref("profilePhotos")
+      .child(filename)
+      .getDownloadURL()
+      .then(url => this.setState({ avatarURL: url }));
+  };
+
   render() {
     const { auth, authError } = this.props;
-
+    const storage = firebase.storage();
+    const storageRef = storage.ref("profilePhotos");
     if (auth.uid) return <Redirect to="/" />;
 
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
           <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <FileUploader
+            accept="image/*"
+            name="avatar"
+            randomizeFilename
+            storageRef={storageRef}
+            onUploadStart={this.handleUploadStart}
+            onUploadError={this.handleUploadError}
+            onUploadSuccess={this.handleUploadSuccess}
+            onProgress={this.handleProgress}
+          />
           <div className="input-field">
             <label htmlFor="userName">Username</label>
             <input type="text" id="userName" onChange={this.handleChange} />
