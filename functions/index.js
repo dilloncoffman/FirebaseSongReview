5fc24328dcaffaee6923c0a806c72e5bd6c3fbdd
Dillon Coffman <tug40394@temple.edu>
2019-11-02 11:26:52 -0400

    Created cloud function for when a review is created to create a 'Now reviewing' notification to be used on the home page; this is also where I will get youtube url and update the firestore videoID field for that review

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,33 @@
 const functions = require("firebase-functions");
-
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+const createNowReviewingNotification = notif => {
+  return admin.firestore
+    .collection("notifications")
+    .add(notif)
+    .then(doc => {
+      console.log("Now Reviewing notification added!");
+    });
+};
+
+exports.reviewCreated = functions.firestore
+  .document("reviews/{reviewID}")
+  .onCreate(doc => {
+    const review = doc.data();
+    const nowReviewingNotification = {
+      content: "Wrote a new review",
+      user: `${review.authorUser}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    // use youtubeLinkToSong property on review to get video id and update the firestore document for that review to hold that video ID
+
+    return createNowReviewingNotification(nowReviewingNotification);
+  });
--- src/home/NowReviewing.js
+++ src/home/NowReviewing.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-const NowReviewing = () => {
+const NowReviewing = props => {
   return (
-    <div>
-      <p>Now Reviewing...</p>
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Now Reviewing</span>
+          <ul className="now-reviewing">
+            <li>Review Notifications</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
